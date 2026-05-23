f957e41415eb3dd44fcc74b24acd275495fc8a85
Abdullah Al Jaber <abdullah.0.al.0.jaber@gmail.com>
2026-05-23 20:46:39 +0600

    OK

--- shell-script/completion.fish
+++ shell-script/completion.fish
@@ -2,7 +2,8 @@
 
 complete -c verse-captor -l start-url -r -d "Start URL for scraping novel chapter"
 complete -c verse-captor -l stop-url -r -d "Stop URL for scraping novel chapter"
-complete -c verse-captor -l folder-path -r -d "Folder path for novel chapter texts"
+complete -c verse-captor -l file-path -r -d "File Path for novel chapter urls"
+complete -c verse-captor -l folder-path -r -d "Folder Path for novel chapter texts"
 complete -c verse-captor -l text-selector -r -d "Selector for extracting text from HTML"
 complete -c verse-captor -l url-selector -r -d "Selector for extracting URL from HTML"
 

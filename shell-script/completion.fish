f2e80ff0758dd10701533b6795d9012dbbb29454
Abdullah Al Jaber <abdullah.0.al.0.jaber@gmail.com>
2026-07-14 21:17:26 +0000

    OK

--- shell-script/completion.fish
+++ shell-script/completion.fish
@@ -5,5 +5,5 @@ complete -c verse-captor -l url-selector -r -d "Selector for extracting URL from
 complete -c verse-captor -l file-path -r -d "File Path for novel chapter urls"
 complete -c verse-captor -l folder-path -r -d "Folder Path for novel chapter texts"
 complete -c verse-captor -l text-selector -r -d "Selector for extracting text from html"
-complete -c verse-captor -l show-locals -r -d "Show local variables for rich.traceback"
+complete -c verse-captor -l show-locals -d "Show local variables for rich.traceback"
 complete -c verse-captor -l help -d "Show help and exit"
